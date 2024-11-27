be6b452e791afc498fa649d72c7bdd406389de54
junil3005 <yjl3005@naver.com>
2024-11-27 12:15:11 +0900

    123

--- src/com/java/app/ContactApp.java
+++ src/com/java/app/ContactApp.java
@@ -1,11 +1,70 @@
 package com.java.app;
 
+import java.util.Scanner;
+
 public class ContactApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean flag = false;	
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		while (true) {
+			
+		if (flag) {
+			System.out.println("프로그램을 종료합니다.");
+			 break;
+			 
+		}
+		System.out.println("-----연락처 프로그램-----");
+		System.out.println("번호를 선택하세요.");
+		System.out.println("0.이름 \n2.전체목록 \n3.부서별 열람 \n4.지사별 열람 \n5.통화 \n6.최근기록 \n7.프로그램 종료 ");
+		int num = scanner.nextInt();
+		
+		switch(num) {
+		case 0 : {
+			while(true) {
+				System.out.println("이름을 입력하세요.");
+				
+				
+			}
+			
+			
+		}
+		
+		case 1 : {
+			while(true) {
+				System.out.println("이름을 입력하세요.");
+				
+//				for ()
+				
+				
+			}
+			
+		}
+		
+		
+		}
+		
+		
+			
+			
+		
+		
+	
+				}
+				
+			}
+			
+		
+			
+			
+		
+		
+		 
+		
 		
 	}
 
-}
+
 
